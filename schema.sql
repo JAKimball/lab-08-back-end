b4af06afb0ffa0d6d812b7958fbf9326cddd5898
jeremiah <jeremiah.kim@hotmail.com>
2019-09-20 07:01:16 -0700

    feature 02 completed

--- schema.sql
+++ schema.sql
@@ -8,9 +8,12 @@ DROP TABLE IF EXISTS locations;
 
 CREATE TABLE locations (
     id SERIAL PRIMARY KEY,
-    searchQuery VARCHAR(255),
+    search_query VARCHAR(255),
+    formatted_query VARCHAR(255),
     latitude FLOAT,
     longitude FLOAT
 );
 
-INSERT INTO locations (searchQuery, latitude, longitude) VALUES ('seattle', 41.1, 41.1);
+INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ('seattle', 'Seattle, WA, USA', 47.6062095, -122.3320708);
+INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ('lynwood', 'Lynwood, CA, USA',33.930293, -118.2114603);
+INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ('chicago', 'Chicago, IL, USA', 41.8781136, -87.6297982);
--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 const client = new pg.Client(process.env.DATABASE_URL);
+console.log(process.env.DATABASE_URL);
 client.on('error', err => console.error(err));
+
 client.connect()
   .then(() => {
     app.listen(PORT, () => console.log(`listening on ${PORT}`));
@@ -26,8 +28,8 @@ client.connect()
  */
 
 app.get('/location', routeLocation);
-// app.get('/weather', getWeather);
-// app.get('/events', getEvents);
+app.get('/weather', getWeather);
+app.get('/events', getEvents);
 app.use('*', wildcardRouter);
 
 /**
@@ -35,76 +37,81 @@ app.use('*', wildcardRouter);
  */
 
 function routeLocation(request, response) {
-  let queryStr = request.query.data;
+  const searchQuery = request.query.data;
 
-  const location = getLocation(queryStr, response);
-  // response.status(200).send(location);
+  try{
+    getLocation(searchQuery, response);
+  }
+  catch(err){
+    handleError(err, response);
+  }
 }
 
-function getLocation(queryStr, response) {
-  let sql = 'SELECT * FROM locations;';
-  let value = [queryStr];
-  console.log('BROKENNNNNNNNN');
+function getLocation(searchQuery, response) {
+  const sql = 'SELECT * FROM locations;';
+
   client
     .query(sql)
-    .then(pgResults => {
-      console.log('====================================================================================================================================================================================');
-      console.log('our pgResults', pgResults);
-      // response.status(200).json(pgResults);
-    })
+    .then(result => {
+      let match = result.rows.filter(city => (city.search_query === searchQuery));
+      match.length > 0 ? delete match[0].id && response.status(200).send(match[0]) : newLocation(searchQuery, response);})
     .catch(err => handleError(err, response));
 }
 
-function googleLocation(queryStr, response){
-  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryStr}&key=${process.env.GEOCODE_API_KEY}`;
+function newLocation(searchQuery, response){
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${process.env.GEOCODE_API_KEY}`;
 
-  superagent.get(url)
-    .then(saResult => {
-      const body = saResult.body;
-      const location = new Location(queryStr, body);
-      response.status(200).send(location);
+  superagent
+    .get(url)
+    .then(result => {
+      const locationData = result.body.results[0];
+      const location = new Location(searchQuery, locationData);
+
+      const sql = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);';
+      const value = [location.search_query, location.formatted_query, location.latitude, location.longitude];
+
+      client
+        .query(sql, value)
+        .then(response.status(200).send(location))
+        .catch(error => handleError(error));
     })
-    .catch(err => handleError(err, response));
 }
 
-function Location(searchQuery, geoDataResults) {
-  const results = geoDataResults.results[0];
-
+function Location(searchQuery, locationData){
   this.search_query = searchQuery;
-  this.formatted_query = results.formatted_address;
-  this.latitude = results.geometry.location.lat;
-  this.longitude = results.geometry.location.lng;
+  this.formatted_query = locationData.formatted_address;
+  this.latitude = locationData.geometry.location.lat;
+  this.longitude = locationData.geometry.location.lng;
 }
 
 function getWeather(request, response) {
   const searchQuery = request.query.data;
   const latitude = searchQuery.latitude;
   const longitude = searchQuery.longitude;
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${latitude},${longitude}`;
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${latitude},${longitude}`
 
-  superagent.get(url)
-    .then(data => {
-      const body = data.body;
-      const forecast = new Forecast(searchQuery, body);
-
-      response.status(200).send(forecast.days);
+  superagent
+    .get(url)
+    .then(results => {
+      const weatherData = results.body.daily.data;
+      const weatherForecast = weatherData.map(dayObj => {
+        return new Weather(dayObj);
+      })
+      response.status(200).send(weatherForecast);
     })
-    .catch(err => handleError(err, response));
+    .catch(error => {
+      handleError(error, response);
+    })
 }
 
-function Forecast(searchQuery, weatherDataResults) {
-  const result = weatherDataResults.daily.data.map(day => {
-    const obj = {};
-    obj.forecast = day.summary;
+function Weather(dayObj){
+  this.forecast = dayObj.summary;
+  this.time = this.formattedDate(dayObj.time);
+}
 
-    const date = new Date(0);
-    date.setUTCSeconds(day.time);
-    obj.time = date.toDateString();
-
-    return obj;
-  });
-
-  this.days = result;
+Weather.prototype.formattedDate = function(time) {
+  let date = new Date(time*1000);
+  return date.toDateString();
 }
 
 function getEvents(request, response) {
@@ -143,9 +150,7 @@ function Error(err) {
 }
 
 function handleError(err, response) {
-  console.log('ERRORE MESSAGE TO FOLOOOWWWWW');
   console.error(err);
-  console.log('ERRORE MESSAGE ENDDDDSSSSS');
   const error = new Error(err);
   if (response) {
     response.status(error.status).send(error.responseText);
